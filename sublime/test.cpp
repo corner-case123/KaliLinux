545f22d6de62487e2587147882c57708cc23b789
corner-case123 <hasanmubtasim1269@gmail.com>
2024-12-20 05:39:07 +0600

    updated 20-12-2024

--- sublime/test.cpp
+++ sublime/test.cpp
@@ -1,25 +1,28 @@
 #include <bits/stdc++.h>
 #define ln endl
-#define ll long long
+#define ull unsigned  long long
+#define ll long long 
 #define vll vector<ll>
 #define pll pair<ll, ll>
 #define ff first
 #define vpll vector<pair<ll,ll>>
+#define pii pair<int,int> 
 #define ss second
 using namespace std;
 
-vector<double> cnt(1e6+1,0) ;
 
 void solve(){
-
+	ll n ; cin >> n ; 
 	return ;
-}
-
+}	
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-
+    ll t = 1; 
+    cin >> t ; 
+    for(ll i=1;i<=t;i++){
+    	cout << "Case " << i << ": " ;  solve() ;
+    }
     return 0;
 }
-
