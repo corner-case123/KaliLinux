eb0877b57390bffc5db45f224a91e72481d41169
corner-case123 <hasanmubtasim1269@gmail.com>
2025-05-16 20:46:54 +0600

    16/5/25

--- sublime/soln.cpp
+++ sublime/soln.cpp
@@ -15,40 +15,17 @@ void setIO(const string io = "a") {
     freopen("a.out", "w", stdout) ;
 }
 
-ll n ;
-vpll ans ;
-set<pll> st ;
-void f(pll p){
-    if (st.find(p)!=st.end()) return ;
-    cout << "? " << p.ff << " " << p.ss  << ln ;
-    ll x  ;  cin >> x ;
-    if (x==p.ff) {
-        ans.push_back({p.ff,p.ss}) ;
-        st.insert({p.ff,p.ss}) ;
-        return  ;
-    }
-    f({x,p.ss}) ;
-}
-
-
-
-
+ll n ; 
 void solve(){
     cin >> n ;
-    ans.clear() ; st.clear() ;
-    for(ll i=2;i<=n;i++){
-        f({1,i}) ;
-    }
-    cout << "! " ;
-    for(auto &p:ans) cout << p.ff << " " << p.ss << " " ;
-    cout << endl ;
+    cout << n << ln ;
 }
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     setIO() ;
-    ll t = 1 ; cin >> t ;
+    ll t = 1 ; //cin >> t ;
     while(t--) solve() ;
     return  0 ;
 }
