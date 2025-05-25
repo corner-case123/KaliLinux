ab25ac0d69b4713eb9dd419f52082499495eb62d
corner-case123 <hasanmubtasim1269@gmail.com>
2025-05-25 23:49:01 +0600

    25/5/25

--- sublime/soln.cpp
+++ sublime/soln.cpp
@@ -1,13 +1,8 @@
 #include <bits/stdc++.h>
-#define ln endl
-#define ull unsigned  long long
-#define ll  long long
-#define vll vector<ll>
-#define ff first
-#define ss second
-#define vpll vector<pair<ll,ll>>
-#define pll pair<ll,ll>
 using namespace std;
+
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0)
+using LL = long long;
 
 
 void setIO(const string io = "a") {
@@ -15,56 +10,44 @@ void setIO(const string io = "a") {
     freopen("a.out", "w", stdout) ;
 }
 
-ll n ; 
-void solve(){
-    cin >> n ;
-    cout << n << ln ;
+
+void pre()
+{
+    fastio ; 
+
+    
 }
 
-// int main() {
-//     ios_base::sync_with_stdio(false);
-//     cin.tie(NULL);
-//     setIO() ;
-//     ll t = 1 ; //cin >> t ;
-//     while(t--) solve() ;
-//     return  0 ;
-// }
-int main(){
-    setIO() ;
-  int a,b,option;
-  printf("Enter the first Integer :");
-  scanf("%d",&a);
-  printf("Enter the second Integer :");
-  scanf("%d",&b);
-  printf("Input your option:\n");
-  printf("1-Addition.\n2-Substraction.\n3-Multiplication.\n4-Division.\n5-Exit.\n");
-  scanf("%d",&option);
+void customSwap(int& a, int& b)
+{
+    if (&a==&b) return ; 
+    a  = a^b ; 
+    b = a^b  ;
+    a =  a^b ;    
+}
 
-  switch(option)
-  {
-    case 1:
-      printf("The Addition of  %d and %d is: %d\n",a,b,a+b);
-      break;
-    case 2:
-      printf("The Substraction of %d  and %d is: %d\n",a,b,a-b);
-      break;
-    case 3:
-      printf("The Multiplication of %d  and %d is: %d\n",a,b,a*b);
-      break;
-    case 4:
-      if(b==0)
-        {
-        printf("The second integer is zero. Divide by zero.\n");
-        }
-      else
+void solve(int tc)
+{
+    int a, b;
+    cin >> a >> b;
+    customSwap(a, a) ;
+    customSwap(a, b);
+    
+    cout << a << "  " << b << '\n';
+}
+
+signed main()
+{
+    pre();
+    setIO() ; 
+    int tc, tt = 1;
+    // cin >> tt;
+    for(tc = 1; tc <= tt; tc++)
     {
-        float div=(float)a/b;
-        printf("The Division of %d  and %d is : %f\n",a,b,div);
-      }
-      break;
-    default:
-      printf("Incorrect option\n");
-      break;
-  }
-  return 0; 
+        // cout << "Case " << tc << ": ";
+        solve(tc);
+        // cout << '\n';
+    }
+
+    return 0;
 }
