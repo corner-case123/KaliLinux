c990c271b359562326292456e70af557c81b4ad1
corner-case123 <mubtasimahan@iut-dhaka.edu>
2025-11-06 09:31:48 +0600

    6/11/25

--- sublime/temp.cpp
+++ sublime/temp.cpp
@@ -1,35 +1,47 @@
 #include <bits/stdc++.h>
 #define ln '\n'
-#define ll long long 
+#define ll long long
 #define pll pair<ll, ll>
 #define vpll vector<pll>
 #define vll vector<ll>
 #define ff first
 #define ss second
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
 using namespace std;
 #define fastio ios_base::sync_with_stdio(0); cin.tie(0)
- template<class T>
-using ordered_set = tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
- 
 
-int palindrome(int i,int j,string s){
-    if (i>j) return 1 ;
-    if (s[i]==s[j]) return palindrome(i+1,j-1,s) ;
-    return 0 ; 
-}
-void solve(){
-    string word = "abccba" ;
-    printf("%d",palindrome(0,5,word)) ;
 
-    return ;
+
+ll s  ;
+void solve() {
+    ll n; cin >>n ;
+    vll a(n) ; for(auto &e:a) cin >> e ;
+    sort(a.begin(),a.end()) ;
+    vll p = a ; for(ll i=1;i<n;i++) p[i]+= p[i-1] ; 
+    if (s==0){
+        if (n%2){
+            cout << accumulate(a.begin(),a.begin()+(n+1)/2,0ll) << endl ;
+        }
+        else {
+            cout << accumulate(a.begin()+1,a.begin()+1+n/2,0ll) << endl ;
+        }
+    }
+    else {
+        ll mx = 0; 
+        for(ll i=n-1;i>=0;i--){
+            ll l = max(i-(n-1-i),0ll) ;
+            mx = max(mx,p[i]-(l==0)?0:p[l-1]) ;
+        }
+        cout << mx << endl ;
+    }
 }
 
 int main() {
     fastio;
-    ll t = 1;  //cin >> t;
-    while(t--) solve();
+    
+    ll t =1 ; cin >> t>> s ;
+    for(ll i=1;i<=t;i++){
+
+        solve() ;
+    }
     return 0;
 }
---
+++ sublime/template.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#define ln '\n'
+#define ll  long long 
+#define pll pair<ll, ll>
+#define vpll vector<pll>
+#define vll vector<ll>
+#define ff first
+#define ss second
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0)
+ //template<class T>
+//using ordered_set = tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+ 
+
+
+//gp_hash_table<long long, long long > mp; (unordered_map)
+// gp_hash_table<long long, null_type> st; (unordered_set)
+// all operation same 
+
+
+
+void solve(){
+
+}
+
+
+int main() {
+    fastio;
+    ll t = 1;  cin >> t;
+    while(t--) solve();
+    return 0;
+}
